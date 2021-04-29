a0fd469995a08f1d5e424651eb287eb95c466c9d
dbalakri <dbalakri@buffalo.edu>
2021-04-29 15:18:05 -0400

    Full run shell script

---
+++ full_run.sh
@@ -0,0 +1,5 @@
+sh ./tpch/tpch_spark_shell_script_1.sh
+sh ./tpch/tpch_spark_shell_script_2.sh
+sh ./tpch/tpch_spark_shell_script_3.sh
+sh ./tpch/tpch_spark_shell_script_4.sh
+sh ./tpch/tpch_spark_shell_script_5.sh
