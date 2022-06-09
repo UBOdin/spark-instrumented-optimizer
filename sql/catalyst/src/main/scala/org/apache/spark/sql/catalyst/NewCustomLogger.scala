5c5b3eb18dd7126fb5fb843ea7373bddaf854e14
Nick Brown <njbrown4@buffalo.edu>
2022-06-09 18:55:24 -0400

    refactored more of `CustomLogger.scala`.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/NewCustomLogger.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/NewCustomLogger.scala
@@ -23,119 +23,254 @@ import scala.collection.mutable.Stack
 
 object CustomLogger
 {
-  val stateTimes: Map[String, Long] = Map[String, Long](
-    "Apply" -> 0,
-    "Transform" -> 0,
-    "Match" -> 0,
-    "Execute" -> 0
-    )
-  val intermediateStack: Stack[String] = Stack[String]()
-  var previousTimeStamp: Long = 0
+    // variables and other global state
+    val descriptorString: String = "stopTheClock"
 
-  def push(state: String): Unit =
-  {
-    if (intermediateStack.isEmpty)
+    val pushString: String = "push"
+    val popString: String = "pop"
+
+    val applyString: String = "Apply"
+    val transformString: String = "Transform"
+    val matchString: String = "Match"
+    val effectiveMatchString: String = "Effective Match"
+    val ineffectiveMatchString: String = "Ineffective Match"
+    val executeString: String = "Execute"
+
+	val stateTimes: Map[String, Long] = Map[String, Long](
+	  applyString -> 0,
+	  transformString -> 0,
+	  matchString -> 0,
+      effectiveMatchString -> 0,
+      ineffectiveMatchString -> 0,
+	  executeString -> 0
+	  )
+
+	val intermediateStateStack: Stack[String] = Stack[String]()
+    val intermediateMatchStack: Stack[Long] = Stack[Long]()
+	var previousTimeStamp: Long = 0
+    
+    val allRulesSet: Set[String] = Set[String]()
+    val effectiveRulesSet: Set[String] = Set[String]()
+    var currentRule: String = ""
+	
+    // logger method definitions
+    def push(state: String): Unit =
+	{
+        val currentTime: Long = System.nanoTime
+        val timeDiff: Long = currentTime - previousTimeStamp
+        if (intermediateStateStack.isEmpty)
+        {
+            intermediateStateStack.push(state)
+            previousTimeStamp = currentTime
+        }
+        else
+        {
+            stateTimes(stack.top) += timeDiff
+            intermediateStateStack.push(state)
+            previousTimeStamp = currentTime
+        }
+	}
+	
+	def pop(state: String, effective: Boolean = false): Unit =
+	{
+        val currentTime: Long = System.nanoTime
+        val timeDiff: Long = currentTime - previousTimeStamp
+        if (intermediateStateStack.isEmpty)
+        {
+            throw new RuntimeException(s"Empty stack! :'$state'")
+        }
+        else if (intermediateStateStack.top != state)
+        {
+            val wrongState: String = intermediateStateStack.top
+            throw new RuntimeException(s"Cannot pop! :'$wrongState' != '$state'")
+        }
+        else
+        {
+            stateTimes(intermediateStateStack.top) += timeDiff
+            intermediateStateStack.pop
+            previousTimeStamp = currentTime
+        }
+	}
+
+    //TODO
+    def checkMatch(action: String, timeDiff: Long, effective: Boolean): Unit =
     {
-      intermediateStack.push(state)
-      previousTimeStamp = System.nanoTime()
+        if (action == pushString)
+        {
+            intermediateMatchStack.push(timeDiff)
+        }
+        else if (action == popString)
+        {
+            if (!intermediateMatchStack.isEmpty)
+            {
+                if (effective)
+                {
+                    stateTimes(effectiveMatchString) = intermediateMatchStack.top + timeDiff
+                    intermediateMatchStack.pop
+                }
+                else
+                {
+                    stateTimes(ineffectiveMatchString) = intermediateMatchStack.top + timeDiff
+                    intermediateMatchStack.pop
+                }
+            }
+            else
+            {
+                throw new RuntimeException("The intermediateMatchStack is empty! cannot pop!")
+            }
+        }
     }
-    else
+    //TODO
+    def logRulesAsSets(ruleName: String, effective: Boolean = false)
     {
-      stateTimes(stack.top) += (System.nanoTime() - previousTimeStamp)
-      intermediateStack.push(state)
-      previousTimeStamp = System.nanoTime()
+        if (effective)
+        {
+            effectiveRulesSet += ruleName
+        }
+
+        allRulesSet += ruleName
     }
-  }
-  
-  def pop(state: String): Unit =
-  {
-    if (intermediateStack.isEmpty)
+
+	//TODO
+	def logApplyTime[F](
+        descriptor: String = "",
+        context: String = "",
+	    log: (String => Unit) =
+	    // scalastyle:off
+	    println(_)
+	    // scalastyle:on
+	    )(anonFunc: => F): F =
+	{
+        push("Apply")
+        val anonFuncRet = anonFunc
+        pop("Apply",)
+        anonFuncRet
+	}
+
+	def logTransformTime[F](
+	    descriptor: String = "",
+	    context: String = "",
+	    log: (String => Unit) =
+	    // scalastyle:off
+	    println(_)
+	    // scalastyle:on
+	)(anonFunc: => F): F =
+	{
+        // Entering a transform.
+        push("Transform")
+        // Looking for rewrites.
+        val anonFuncRet = anonFunc
+        // Exiting a transform.
+        pop("Transform")
+        anonFuncRet
+	}
+
+	def logMatchTime[F](
+	    descriptor: String = "",
+	    unAffected: Boolean = false,
+	    context: String = "",
+	    log: (String => Unit) =
+	    // scalastyle:off
+	    println(_)
+	    // scalastyle:on
+	)(anonFunc: => F): F =
+	{
+        // Entering a match.
+        push("Match")
+        // Applying the rewrite
+        val anonFuncRet = anonFunc
+        // Exiting a match.
+        pop("Match")
+        anonFuncRet
+	}
+
+	def logExecutionTime[F](
+	    descriptor: String = "",
+	    context: String = "",
+	    log: (String => Unit) =
+	    // scalastyle:off
+	    println(_)
+	    // scalastyle:on
+	)(anonFunc: => F): F =
+	{
+        // Entering execution.
+        push("Execute")
+        // Run execution.
+        val anonFuncRet = anonFunc
+        // Exit execution.
+        pop("Execute")
+        anonFuncRet
+	}
+
+	//TODO
+	def printData(
+        printJSON: Boolean = true,
+        printHumanReadable: Boolean = false,
+        printASTSize: Boolean = false,
+        printAllRules: Boolean = false,
+        printEffectiveRules: Boolean = false
+    ): Unit =
     {
-      throw new RuntimeException(s"Empty stack! :'$state'")
+        val applyTime: Long = stateTimes.getOrElse(applyString, -27)
+        val transformTime: Long = stateTimes.getOrElse(transformString, -27)
+        val matchTime: Long = stateTimes.getOrElse(matchString, -27)
+        val effectiveMatchTime: Long = stateTimes.getOrElse(effectiveMatchString, -27)
+        val inefffectiveMatchTime: Long = stateTimes.getOrElse(ineffectiveMatchString, -27)
+        val executeTime: Long = stateTimes.getOrElse(executeString, -27)
+        
+        if (printJSON)
+        
+            // scalastyle:off
+            print(s"""
+                ------------------------------\n
+                {"data":{"applyTime": $applyTime, "transformTime": $transformTime, "matchTime": $matchTime, "effectiveMatchTime": $effectiveMatchTime, "inefffectiveMatchTime: $effectiveMatchTime, "executeTime": $executeTime}}\n
+                ------------------------------\n
+                """)
+            // scalastyle:on
+        }
+        
+        if (printHumanReadable)
+        {
+            // scalastyle:off
+            print(s"""
+                ------------------------------\n
+                humanreadableformat\n
+                ------------------------------\n
+                """)
+            // scalastyle:on
+        }
+        
+        if (printASTSize)
+        {
+            // scalastyle:off
+            print(s"""
+                ------------------------------\n
+                astsize\n
+                ------------------------------\n
+                """)
+            // scalastyle:on
+        }
+        
+        if (printAllRulesSet)
+        {
+            // scalastyle:off
+            print(s"""
+                ------------------------------\n
+                allrules\n
+                ------------------------------\n
+                """)
+            // scalastyle:on
+        }
+
+        if (printEffectiveRules)
+        {
+            // scalastyle:off
+            print(s"""
+                ------------------------------\n
+                effectiverules\n
+                ------------------------------\n
+                """)
+            // scalastyle:on
+        }
     }
-    else if (intermediateStack.top != state)
-    {
-      val wrongState: String = intermediateStack.top
-      throw new RuntimeException(s"Cannot pop! :'$wrongState' != '$state'")
-    }
-    else
-    {
-      stateTimes(intermediateStack.top) += (System.nanoTime() - previousTimeStamp)
-      intermediateStack.pop
-      previousTimeStamp = System.nanoTime()
-    }
-  }
-
-  //TODO
-  def logApplyTime()
-  {
-
-  }
-  
-  def logTransformTime[F](
-  descriptor: String,
-  context: String = "",
-  log: (String => Unit) =
-  // scalastyle:off
-  println(_)
-  // scalastyle:on
-  )(anonFunc: => F): F =
-  {
-    // Entering a transform.
-    push("Transform")
-    // Looking for rewrites.
-    val anonFuncRet = anonFunc
-    // Exiting a transform.
-    pop("Transform")
-    anonFuncRet
-  }
-
-  def logMatchTime[F](
-  descriptor: String,
-  unAffected: Boolean,
-  context: String = "",
-  log: (String => Unit) =
-  // scalastyle:off
-  println(_)
-  // scalastyle:on
-  )(anonFunc: => F): F =
-  {
-    // Entering a match.
-    push("Match")
-    // Applying the rewrite
-    val anonFuncRet = anonFunc
-    // Exiting a match.
-    pop("Match")
-    anonFuncRet
-  }
-
-  def logExecutionTime[F](
-  descriptor: String,
-  context: String = "",
-  log: (String => Unit) =
-  // scalastyle:off
-  println(_)
-  // scalastyle:on
-  )(anonFunc: => F): F =
-  {
-    // Entering execution.
-    push("Execute")
-    // Run execution.
-    val anonFuncRet = anonFunc
-    // Exit execution.
-    pop("Execute")
-    anonFuncRet
-  }
-
-  //TODO
-  def pushEffectiveRuleCheck(): Unit =
-  {
-
-  }
-
-  //TODO
-  def popEffectiveRuleCheck(): Unit =
-  {
-
-  }
 }
